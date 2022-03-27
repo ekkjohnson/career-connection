1c1b3b69d20d46c7ccd6347635ca6763e1309967
Elizabeth <ekkjohnson@gmail.com>
2022-03-27 10:22:03 -0500

    questions

--- assets/javaScript/script1.js
+++ assets/javaScript/script1.js
@@ -34,8 +34,39 @@ function buildQuiz(){}
 
 function showResults(){}
 
-// display quiz right away
+// display quiz 
 buildQuiz();
 
-// on submit, show results
+// show results
 submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+      question: "Which is not a data type?",
+      answers: {
+        a: "alert",
+        b: "boolean",
+        c: "string"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is an array inclosed in?",
+      answers: {
+        a: "curly braces {}",
+        b: "square brackets []",
+        c: "parenthases ()"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Which of the following is a boolean value?",
+      answers: {
+        a: "true",
+        b: "0",
+        c: "1",
+        d: "all of the above"
+      },
+      correctAnswer: "d"
+    }
+  ];
