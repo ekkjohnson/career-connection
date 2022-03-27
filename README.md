591b059a517f533a2728c1d89574475c07a9be26
Elizabeth <ekkjohnson@gmail.com>
2022-03-27 09:49:14 -0500

    timer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # code-quiz
+A quiz to guess letters from blanks to spell a word, similar to hangman
+The words are related to coding
+Has a timer that counts down
--- assets/javaScript/script1.js
+++ assets/javaScript/script1.js
@@ -1,7 +1,27 @@
-var welcomeText= document.querySelector('.lrgTitle')
-var rules= document.querySelector('.intro-Rules')
-var beginGame= document.querySelector('.into-Rules')
-var timeLeft= 3
-var blank=document.querySelector('#blankSpace')
-var timeDisplays= document.querySelector('timerStart')
-var quest=document.querySelector('questions')
+var rooEl=$("#root");
+
+var welcomeText = document.querySelector('.lrgTitle')
+var rules = document.querySelector('.intro-Rules')
+var beginGame = document.querySelector('.into-Rules')
+var timeLeft = 10
+var blank = document.querySelector('#blankSpace')
+var timeDisplay = document.querySelector('.timerStart')
+var quest = document.querySelector('questions')
+
+beginGame.addEventListener('click', function () {
+
+    var timer = setInterval(function () {
+        timeLeft--;
+        if (timeLeft < 0) {
+            clearInterval(timer)
+        }
+        else if (timeLeft === 0) {
+            alert('Game Over!')
+            document.textcontent(timeDisplay) = ("timeLeft");
+            
+        }
+        else (
+            timeDisplay.textContent = blank
+        
+     )} , 1000)
+})
