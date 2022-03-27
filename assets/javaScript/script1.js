8d5b15b15452224840c6bd84cedc648ffbfc5c7a
Elizabeth <ekkjohnson@gmail.com>
2022-03-27 10:15:31 -0500

    js quiz

--- assets/javaScript/script1.js
+++ assets/javaScript/script1.js
@@ -25,3 +25,17 @@ beginGame.addEventListener('click', function () {
         
      )} , 1000)
 })
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
