0622ac80f447bab4c0bcafa036ec28417f5d21df
Elizabeth <ekkjohnson@gmail.com>
2022-03-27 11:30:16 -0500

    fixes

--- assets/javaScript/script1.js
+++ assets/javaScript/script1.js
@@ -2,28 +2,30 @@ var rooEl=$("#root");
 
 var welcomeText = document.querySelector('.lrgTitle')
 var rules = document.querySelector('.intro-Rules')
-var beginGame = document.querySelector('.into-Rules')
+var beginGame = document.querySelector('.start-btn')
 var timeLeft = 10
 var blank = document.querySelector('#blankSpace')
-var timeDisplay = document.querySelector('.timerStart')
+var timeDisplay = document.querySelector('timerStart')
 var quest = document.querySelector('questions')
 
 beginGame.addEventListener('click', function () {
-
     var timer = setInterval(function () {
         timeLeft--;
         if (timeLeft < 0) {
             clearInterval(timer)
         }
         else if (timeLeft === 0) {
-            alert('Game Over!')
-            document.textcontent(timeDisplay) = ("timeLeft");
-            
+            // alert('Game Over!')
+            // document.textContent(timeDisplay) = (timeLeft);
+            timeDisplay.textContent= (timeLeft)
+            timeDisplay.textContent = (" ")
         }
         else (
-            timeDisplay.textContent = blank
-        
-     )} , 1000)
+        // document.textContent(timeDisplay) = (timeLeft);
+        timeDisplay.textContent= (timeLeft)
+        )
+         } , 1000)
+    
 })
 
 const quizContainer = document.getElementById('quiz');
@@ -40,33 +42,39 @@ buildQuiz();
 // show results
 submitButton.addEventListener('click', showResults);
 
-const myQuestions = [
-    {
-      question: "Which is not a data type?",
-      answers: {
-        a: "alert",
-        b: "boolean",
-        c: "string"
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "What is an array inclosed in?",
-      answers: {
-        a: "curly braces {}",
-        b: "square brackets []",
-        c: "parenthases ()"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "Which of the following is a boolean value?",
-      answers: {
-        a: "true",
-        b: "0",
-        c: "1",
-        d: "all of the above"
-      },
-      correctAnswer: "d"
-    }
-  ];
+
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+  
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+  
+        // variable to store the list of possible answers
+        const answers = [];
+  
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+  
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+  
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  }
