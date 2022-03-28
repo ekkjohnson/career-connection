9d7b46f9169b575bd8e27f7569952379e6df231d
Elizabeth <ekkjohnson@gmail.com>
2022-03-28 12:31:12 -0500

    js changes

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,198 +1,129 @@
-var rooEl=$("#root");
+var rooEl = $("#root");
 
-var welcomeText = document.querySelector(".lrgTitle")
-var rules = document.querySelector(".intro-Rules")
-var beginGame = document.getElementById("start-btn")
-var timeLeft = 10
-var blank = document.querySelector("#blankSpace")
-var timeDisplay = document.querySelector(".timerStart")
-var quest = document.querySelector(".questions")
+var welcomeText = document.querySelector(".lrgTitle");
+var rules = document.querySelector(".intro-Rules");
+var beginGame = document.querySelector(".start-btn");
+var timeLeft = 10;
+var blank = document.querySelector("#blankSpace");
+var timeDisplay = document.getElementById("timer-start");
+var quest = document.querySelector("questions");
 
-beginGame.addEventListener('click', function countDown() {
-    // window.alert("hi")
-    questionContainer.style.displays="block"
-    showQuestion(numCorrect)
-    timer = setInterval(function () {
-        timeLeft--;
-        timerDisplay.textContent= timeLeft
-        if (timeLeft < 0) {
-            clearInterval(timer)
-        }
-        else if (timeLeft === 0) {
-            alert('Game Over!')
-            timeDisplay.textContent= timeLeft
-            timeLeft.textContent = ""
-        }
-        else (
-            timeDisplay.textContent = timeLeft
-        
-     )} , 1000)
-        console.log(timeLeft) 
-})
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
 
-// function startTimer() {
-//     // Sets timer
-//     timer = setInterval(function() {
-//       timerCount--;
-//       timerElement.textContent = timerCount;
-//       if (timerCount >= 0) {
-//         // Tests if win condition is met
-//         if (isWin && timerCount > 0) {
-//           // Clears interval and stops timer
-//           clearInterval(timer);
-//         //   winGame();
-//         }
-//       }
-//       // Tests if time has run out
-//       if (timerCount === 0) {
-//         // Clears interval
-//         clearInterval(timer);
-//         // loseGame();
-//       }
-//     }, 1000);
-//   }
+beginGame.addEventListener("click", function () {
+  var timer = setInterval(function () {
+    console.log(timeLeft);
+    timeLeft--;
+    if (timeLeft < 0) {
+      clearInterval(timer);
+    } else if (timeLeft === 0) {
+      alert("Game Over!");
+      timeDisplay.textContent = timeLeft;
+      timeLeft.textContent = "";
+    } else timeDisplay.textContent = timeLeft;
+  }, 1000);
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-{<script type="text/javascript" src="./assets/javaScript/script2.js"></script>}
+  console.log(timeDisplay.textContent);
+});
 
-// const myQuestions = [
-//     {
-//       question: "Which is not a data type?",
-//       answers: {
-//         a: "alert",
-//         b: "boolean",
-//         c: "string"
-//       },
-//       correctAnswer: "a"
-//     },
-//     {
-//       question: "What is an array inclosed in?",
-//       answers: {
-//         a: "curly braces {}",
-//         b: "square brackets []",
-//         c: "parenthases ()"
-//       },
-//       correctAnswer: "b"
-//     },
-//     {
-//       question: "Which of the following is a boolean value?",
-//       answers: {
-//         a: "true",
-//         b: "0",
-//         c: "1",
-//         d: "all of the above"
-//       },
-//       correctAnswer: "d"
-//     }
-//   ];
+function buildQuiz() {}
 
-function buildQuiz(){}
+function showResults() {}
 
-function showResults(){}
-
-// display quiz 
+// display quiz
 buildQuiz();
 
 // show results
-submitButton.addEventListener('click', showResults);
+submitButton.addEventListener("click", showResults);
 
+function buildQuiz() {
+  // variable to store the HTML output
+  const output = [];
 
-  function buildQuiz(){
-    // variable to store the HTML output
-    const output = [];
-  
-    // for each question...
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
-  
-        // variable to store the list of possible answers
-        const answers = [];
-  
-        // and for each available answer...
-        for(letter in currentQuestion.answers){
-  
-          // ...add an HTML radio button
-          answers.push(
-            `<label>
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // variable to store the list of possible answers
+    const answers = [];
+
+    // and for each available answer...
+    for (letter in currentQuestion.answers) {
+      // ...add an HTML radio button
+      answers.push(
+        `<label>
               <input type="radio" name="question${questionNumber}" value="${letter}">
               ${letter} :
               ${currentQuestion.answers[letter]}
             </label>`
-          );
-        }
-  
-        // add this question and its answers to the output
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
-      }
+      );
+    }
+
+    // add this question and its answers to the output
+    output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>`
     );
-  
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join('');
-    showSlide(currentSlide);
-    function showNextSlide() {
-        showSlide(currentSlide + 1);
-      }
-      
-      function showPreviousSlide() {
-        showSlide(currentSlide - 1);
-      }
-      previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
+  });
 
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join("");
+  showSlide(currentSlide);
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
   }
 
-
-  function showResults(){
-
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-    // keep track of user's answers
-    let numCorrect = 0;
-  
-    // for each question...
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-      // if answer is correct
-      if(userAnswer === currentQuestion.correctAnswer){
-        // add to the number of correct answers
-        numCorrect++;
-  
-        // color the answers green
-        answerContainers[questionNumber].style.color = 'lightgreen';
-      }
-      // if answer is wrong or blank
-      else{
-        // color the answers red
-        answerContainers[questionNumber].style.color = 'red';
-      }
-    });
-  
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
   }
-  var profile = [];
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+}
 
-userStore.addEventListener("click", function(event) {
-  event.preventDefault();
-  
-  // TODO: Create user object from submissio
-var userProfile ={
-intials: initialsInput.value,
-//   score:
+function showResults() {
+  // gather answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll(".answers");
 
-  // TODO: Set new submission to local storage 
-//   localStorage.setItem("userProfile", JSON.stringify(userProfile))
+  // keep track of user's answers
+  let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach((currentQuestion, questionNumber) => {
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if (userAnswer === currentQuestion.correctAnswer) {
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = "lightgreen";
+    }
+    // if answer is wrong or blank
+    else {
+      // color the answers red
+      answerContainers[questionNumber].style.color = "red";
+    }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+var profile = [];
+
+// userStore.addEventListener("click", function(event) {
+//   event.preventDefault();
+
+//   // TODO: Create user object from submissio
+// var userProfile ={
+// intials: initialsInput.value,
+
+// }
+//   // TODO: Set new submission to local storage
+//   localStorage.setItem("userProfile", JSON.stringify(userProfile));
 //   renderMessage();
-}
-}
+
+// }
