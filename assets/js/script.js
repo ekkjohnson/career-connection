cb230a00104274c6d2be7af66189d7dd51898f91
Elizabeth <ekkjohnson@gmail.com>
2022-03-28 22:03:37 -0500

    help

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var rules= document.querySelector('.intro-Rules')
 var beginGame= document.querySelector('.start-btn'); beginGame.style.position = "sticky";
 beginGame.style.left = "50%";
 beginGame.style.transform = "translateX(-50%)";
-var timeLeft= 100
+var timeLeft= 10
 var timeDisplay= document.querySelector('.timerStart')
 var quest= document.querySelector('#questions')
 var container= document.querySelector('#content')
@@ -118,92 +118,7 @@ beginGame.addEventListener("click", function () {
   console.log(timeDisplay.textContent);
 });
 
-// function buildQuiz() {}
 
-// function showResults() {}
-
-// // display quiz
-// buildQuiz();
-
-// // show results
-// submitButton.addEventListener("click", showResults);
-
-// function buildQuiz() {
-//   // variable to store the HTML output
-//   const output = [];
-
-//   // for each question...
-//   questions.forEach((currentQuestion, questionNumber) => {
-//     // variable to store the list of possible answers
-//     const answers = [];
-
-//     // and for each available answer...
-//     for (letter in currentQuestion.answers) {
-//       // ...add an HTML radio button
-//       answers.push(
-//         `<label>
-//               <input type="radio" name="question${questionNumber}" value="${letter}">
-//               ${letter} :
-//               ${currentQuestion.answers[letter]}
-//             </label>`
-//       );
-//     }
-
-//     // add this question and its answers to the output
-//     output.push(
-//       `<div class="question"> ${currentQuestion.question} </div>
-//           <div class="answers"> ${answers.join("")} </div>`
-//     );
-//   });
-
-  // finally combine our output list into one string of HTML and put it on the page
-//   quizContainer.questions.js = output.join("");
-//   showSlide(currentSlide);
-//   function showNextSlide() {
-//     showSlide(currentSlide + 1);
-//   }
-
-//   function showPreviousSlide() {
-//     showSlide(currentSlide - 1);
-//   }
-//   previousButton.addEventListener("click", showPreviousSlide);
-//   nextButton.addEventListener("click", showNextSlide);
-// }
-
-
-
-// function showResults() {
-//   // gather answer containers from our quiz
-//   const answerContainers = quizContainer.querySelectorAll(".answers");
-
-//   // keep track of user's answers
-//   let numCorrect = 0;
-
-//   // for each question...
-//   myQuestions.forEach((currentQuestion, questionNumber) => {
-//     // find selected answer
-//     const answerContainer = answerContainers[questionNumber];
-//     const selector = `input[name=question${questionNumber}]:checked`;
-//     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-//     // if answer is correct
-//     if (userAnswer === currentQuestion.correctAnswer) {
-//       // add to the number of correct answers
-//       numCorrect++;
-
-//       // color the answers green
-//       answerContainers[questionNumber].style.color = "lightgreen";
-//     }
-//     // if answer is wrong or blank
-//     else {
-//       // color the answers red
-//       answerContainers[questionNumber].style.color = "red";
-//     }
-//   });
-
-//   // show number of correct answers out of total
-//   resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-// }
 
 var user_initial_input=document.querySelector('#intial')
 var scoreInput=document.querySelector('#score')
@@ -236,3 +151,18 @@ saveBtnInput.addEventListener("click", function(event){
   console.log(records)
 }
 )
+function renderMessage(playerRecords) {
+  var playerListFromStorage = JSON.parse(localStorage.getItem("userRecord"));  
+ 
+ 
+  for (var i = 0; i < playerListFromStorage.length; i++) {
+      console.log(playerListFromStorage[userRecord]);
+      allData += playerListFromStorage[userRecord];
+  }
+  var allData = "";
+  
+  document.querySelector(".scoreInfo").innerHTML = allData;
+     }
+//element to display questions
+const questTitle= document.createElement("h3");
+quest.appendChild(questTitle).textContent= ('QUESTIONS')
